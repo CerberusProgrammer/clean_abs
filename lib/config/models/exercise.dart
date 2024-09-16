e09c56b98929948a0993ed55a7eab4648040fa78
Omar Flores <cerberusprogrammer@gmail.com>
2024-09-16 15:09:02 -0700

    Add Exercise model for storing exercise details

---
+++ lib/config/models/exercise.dart
@@ -0,0 +1,15 @@
+class Exercise {
+  String name;
+  String description;
+  String image;
+  int repetitions;
+  int? duration;
+
+  Exercise({
+    required this.name,
+    required this.description,
+    required this.image,
+    required this.repetitions,
+    required this.duration,
+  });
+}

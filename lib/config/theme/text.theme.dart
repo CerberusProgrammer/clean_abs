d48cabec8c6a58e1d52c1da0c7f2f734029e394a
Omar Flores <cerberusprogrammer@gmail.com>
2024-09-16 15:09:19 -0700

    Refactor text theme in text.theme.dart

---
+++ lib/config/theme/text.theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextTheme textTheme = GoogleFonts.rubikTextTheme(const TextTheme(
+  displayLarge: TextStyle(fontSize: 30, color: Colors.white),
+  displayMedium: TextStyle(fontSize: 20, color: Colors.white),
+  displaySmall: TextStyle(fontSize: 15, color: Colors.white),
+  labelLarge: TextStyle(fontSize: 20, color: Colors.white),
+  labelMedium: TextStyle(fontSize: 12, color: Colors.white),
+  labelSmall: TextStyle(fontSize: 10, color: Colors.white),
+  bodyLarge: TextStyle(fontSize: 30, color: Colors.white),
+  bodyMedium: TextStyle(fontSize: 20, color: Colors.white),
+  bodySmall: TextStyle(fontSize: 15, color: Colors.white),
+  titleLarge:
+      TextStyle(fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),
+  titleMedium:
+      TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+  titleSmall:
+      TextStyle(fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold),
+));

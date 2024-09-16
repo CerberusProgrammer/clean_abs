062bd5894a68a7ef57582231314217ebaef0267c
Omar Flores <cerberusprogrammer@gmail.com>
2024-09-16 15:09:14 -0700

    Refactor color theme in color.theme.dart

---
+++ lib/config/theme/color.theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const ColorScheme colorScheme = ColorScheme(
+  primary: Color(0xFFFF0055),
+  secondary: Color(0xFF14151A),
+  surface: Color(0xFF262626),
+  error: Colors.red,
+  onPrimary: Colors.white,
+  onSecondary: Colors.white,
+  onSurface: Colors.white,
+  onError: Colors.white,
+  brightness: Brightness.dark,
+);

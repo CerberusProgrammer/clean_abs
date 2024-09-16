8dd92463a6c686ff7a462c71a99913a8b9deea12
Omar Flores <cerberusprogrammer@gmail.com>
2024-09-16 15:43:50 -0700

    Refactor font sizes in text.theme.dart

--- lib/config/theme/text.theme.dart
+++ lib/config/theme/text.theme.dart
@@ -1,20 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-TextTheme textTheme = GoogleFonts.rubikTextTheme(const TextTheme(
-  displayLarge: TextStyle(fontSize: 30, color: Colors.white),
-  displayMedium: TextStyle(fontSize: 20, color: Colors.white),
-  displaySmall: TextStyle(fontSize: 15, color: Colors.white),
-  labelLarge: TextStyle(fontSize: 20, color: Colors.white),
-  labelMedium: TextStyle(fontSize: 12, color: Colors.white),
-  labelSmall: TextStyle(fontSize: 10, color: Colors.white),
-  bodyLarge: TextStyle(fontSize: 24, color: Colors.white),
-  bodyMedium: TextStyle(fontSize: 18, color: Colors.white),
-  bodySmall: TextStyle(fontSize: 14, color: Colors.white),
-  titleLarge:
-      TextStyle(fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),
-  titleMedium:
-      TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
-  titleSmall:
-      TextStyle(fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold),
-));
+TextTheme textTheme = GoogleFonts.rubikTextTheme(
+  const TextTheme(
+    displayLarge: TextStyle(
+      fontSize: 30,
+      color: Colors.white,
+    ),
+    displayMedium: TextStyle(
+      fontSize: 20,
+      color: Colors.white,
+    ),
+    displaySmall: TextStyle(
+      fontSize: 15,
+      color: Colors.white,
+    ),
+    labelLarge: TextStyle(
+      fontSize: 20,
+      color: Colors.white,
+    ),
+    labelMedium: TextStyle(
+      fontSize: 12,
+      color: Colors.white,
+    ),
+    labelSmall: TextStyle(
+      fontSize: 10,
+      color: Colors.white,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 22,
+      color: Colors.white,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 16,
+      color: Colors.white,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 14,
+      color: Colors.white,
+    ),
+    titleLarge: TextStyle(
+      fontSize: 30,
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+    ),
+    titleMedium: TextStyle(
+      fontSize: 20,
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+    ),
+    titleSmall: TextStyle(
+      fontSize: 15,
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+);

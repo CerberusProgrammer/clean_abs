96659f4f69367dfafa2c213e42581ceac101d976
cerberusprogrammer <cerberusprogrammer@gmail.com>
2024-09-17 23:15:36 +0000

    Splash screen

---
+++ lib/routine.get_ready.screen.dart
@@ -0,0 +1,14 @@
+import 'package:clean_abs/config/theme/color.theme.dart';
+import 'package:flutter/material.dart';
+
+class RoutineGetReadyScreen extends StatelessWidget {
+  const RoutineGetReadyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      color: colorScheme.primary,
+      
+    );
+  }
+}

870c33a37e63f437559ef5d1d40a5998fa2b3add
cerberusprogrammer <cerberusprogrammer@gmail.com>
2024-09-17 14:28:24 +0000

    a generic layout to see the app

---
+++ lib/home.layout.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomeLayout extends StatelessWidget {
+  final Widget children;
+
+  const HomeLayout({super.key, required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Clean Abs'),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+        ],
+      ),
+    );
+  }
+}

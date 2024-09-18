933c309e35ba15f81b89ccc00a24a6922e137204
cerberusprogrammer <cerberusprogrammer@gmail.com>
2024-09-18 14:20:26 +0000

    New route to workout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:clean_abs/config/theme/theme.dart';
 import 'package:clean_abs/home.screen.dart';
 import 'package:clean_abs/routine.screen.dart';
+import 'package:clean_abs/routine.view.dart';
 import 'package:flutter/material.dart';
 
  void main() => runApp(const MainApp());
@@ -14,14 +15,23 @@ class MainApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: themeData,
         onGenerateRoute: (settings) {
-          if (settings.name!.startsWith('/routine/')) {
-            final id = settings.name!.replaceFirst('/routine/', '');
-            return MaterialPageRoute(
-              builder: (context) => RoutineScreen(id: id),
-            );
-          }
-          return null;
-        },
+  if (settings.name!.startsWith('/routine/')) {
+    final parts = settings.name!.split('/'); 
+    if (parts.length == 4 && parts[3] == 'workout') {
+      final id = parts[2];
+      return MaterialPageRoute(
+        builder: (context) => RoutineView(id: id),
+      );
+    } else if (parts.length == 3) {
+      final id = parts[2];
+      return MaterialPageRoute(
+        builder: (context) => RoutineScreen(id: id),
+      );
+    }
+  }
+  return null;
+},
+
         routes: {
           '/': (context) => const HomeScreen(),
         });

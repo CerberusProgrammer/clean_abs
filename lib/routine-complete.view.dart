39a1c199f5131dc317582b33304947ecb2072273
sazarcode <cerberusprogrammer@gmail.com>
2024-09-22 15:33:03 -0700

    Refactor RoutineCompleteView widget and improve UI layout

--- lib/routine-complete.view.dart
+++ lib/routine-complete.view.dart
@@ -1,5 +1,6 @@
 import 'package:clean_abs/config/models/exercise_stat.dart';
 import 'package:clean_abs/config/models/routine_stat.dart';
+import 'package:clean_abs/config/theme/color.theme.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:go_router/go_router.dart';
@@ -12,21 +13,23 @@ class RoutineCompleteView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            RoutineInfo(routineStat: routineStat),
-            const SizedBox(height: 20),
-            ExerciseStatsList(exerciseStats: routineStat.exerciseStats),
-          ],
+      appBar: AppBar(
+        title: const Text('Routine Complete'),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(130),
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 16.0),
+            child: RoutineInfo(routineStat: routineStat),
+          ),
         ),
+        backgroundColor: colorScheme.primary,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ExerciseStatsList(exerciseStats: routineStat.exerciseStats),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.go('/');
-        },
+        onPressed: () => context.go('/'),
         child: const Icon(Icons.done),
       ),
     );
@@ -40,28 +43,25 @@ class ExerciseStatsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: exerciseStats.length,
-        itemBuilder: (context, index) {
-          final exerciseStat = exerciseStats[index];
-          return Padding(
-            padding: const EdgeInsets.only(bottom: 8.0),
-            child: ListTile(
-              title: Text(exerciseStat.exercise.name),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Duration: ${exerciseStat.duration.inSeconds} seconds'),
-                  Text(
-                      'Start Time: ${_formatDateTime(exerciseStat.startTime)}'),
-                  Text('End Time: ${_formatDateTime(exerciseStat.endTime)}'),
-                ],
-              ),
+    return ListView.builder(
+      itemCount: exerciseStats.length,
+      itemBuilder: (context, index) {
+        final exerciseStat = exerciseStats[index];
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 8.0),
+          child: ListTile(
+            title: Text(exerciseStat.exercise.name),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Duration: ${exerciseStat.duration.inSeconds} seconds'),
+                Text('Start Time: ${_formatDateTime(exerciseStat.startTime)}'),
+                Text('End Time: ${_formatDateTime(exerciseStat.endTime)}'),
+              ],
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 
@@ -82,30 +82,25 @@ class RoutineInfo extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text('Routine: ${routineStat.routine.name}',
-            style: const TextStyle(fontSize: 24)),
-        const SizedBox(height: 10),
-        Text(
-          'Total Duration: ${routineStat.totalDuration.inMinutes} minutes',
-          style: const TextStyle(fontSize: 18),
-        ),
-        Text(
-          'Calories Burned: ${routineStat.caloriesBurned}',
-          style: const TextStyle(fontSize: 18),
-        ),
-        Text(
-          'Start Time: ${_formatDateTime(routineStat.startTime)}',
-          style: const TextStyle(fontSize: 18),
-        ),
-        Text(
-          'End Time: ${_formatDateTime(routineStat.endTime)}',
-          style: const TextStyle(fontSize: 18),
-        ),
-      ],
-    );
-  }
+  Widget build(BuildContext context) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Total Duration: ${routineStat.totalDuration.inMinutes} minutes',
+            style: const TextStyle(fontSize: 18),
+          ),
+          Text(
+            'Calories Burned: ${routineStat.caloriesBurned}',
+            style: const TextStyle(fontSize: 18),
+          ),
+          Text(
+            'Start Time: ${_formatDateTime(routineStat.startTime)}',
+            style: const TextStyle(fontSize: 18),
+          ),
+          Text(
+            'End Time: ${_formatDateTime(routineStat.endTime)}',
+            style: const TextStyle(fontSize: 18),
+          ),
+        ],
+      );
 }

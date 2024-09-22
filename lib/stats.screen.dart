61abfd5e42b5d9cf0a91de30274d1e5a77427b80
Omar Flores <cerberusprogrammer@gmail.com>
2024-09-22 11:48:48 -0700

    Refactor StatsScreen to display routine stats and handle empty state

--- lib/stats.screen.dart
+++ lib/stats.screen.dart
@@ -1,18 +1,37 @@
+import 'package:clean_abs/providers/routine_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class StatsScreen extends StatelessWidget {
+class StatsScreen extends ConsumerWidget {
   const StatsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final routineStats = ref.watch(routineStatsProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Stats'),
       ),
-      body: const Center(
-        child: Text('Stats Screen'),
-      ),
+      body: routineStats.isEmpty
+          ? const Center(
+              child: Text('No routines completed yet'),
+            )
+          : ListView.builder(
+              padding: const EdgeInsets.all(16.0),
+              itemCount: routineStats.length,
+              itemBuilder: (context, index) {
+                final routineStat = routineStats[index];
+                return Card(
+                  child: ListTile(
+                    title: Text(routineStat.routine.name),
+                    subtitle: Text(
+                        'Completed on ${routineStat.endTime.toLocal().toString().split(' ')[0]}'),
+                  ),
+                );
+              },
+            ),
       bottomNavigationBar: NavigationBar(
         selectedIndex: 1,
         onDestinationSelected: (index) {

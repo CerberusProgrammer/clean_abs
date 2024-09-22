d93bb7772e8b1e923e71344acadadb5c1755cd10
sazarcode <cerberusprogrammer@gmail.com>
2024-09-21 17:41:02 -0700

    Refactor RoutineView to include RoutineTimerView and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:clean_abs/config/theme/theme.dart';
 import 'package:clean_abs/home.screen.dart';
 import 'package:clean_abs/routine.screen.dart';
-import 'package:clean_abs/routine.view.dart';
+import 'package:clean_abs/routine-timer.view.dart';
 import 'package:flutter/material.dart';
 
- void main() => runApp(const MainApp());
+void main() => runApp(const MainApp());
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -15,23 +15,22 @@ class MainApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: themeData,
         onGenerateRoute: (settings) {
-  if (settings.name!.startsWith('/routine/')) {
-    final parts = settings.name!.split('/'); 
-    if (parts.length == 4 && parts[3] == 'workout') {
-      final id = parts[2];
-      return MaterialPageRoute(
-        builder: (context) => RoutineView(id: id),
-      );
-    } else if (parts.length == 3) {
-      final id = parts[2];
-      return MaterialPageRoute(
-        builder: (context) => RoutineScreen(id: id),
-      );
-    }
-  }
-  return null;
-},
-
+          if (settings.name!.startsWith('/routine/')) {
+            final parts = settings.name!.split('/');
+            if (parts.length == 4 && parts[3] == 'workout') {
+              final id = parts[2];
+              return MaterialPageRoute(
+                builder: (context) => RoutineView(id: id),
+              );
+            } else if (parts.length == 3) {
+              final id = parts[2];
+              return MaterialPageRoute(
+                builder: (context) => RoutineScreen(id: id),
+              );
+            }
+          }
+          return null;
+        },
         routes: {
           '/': (context) => const HomeScreen(),
         });

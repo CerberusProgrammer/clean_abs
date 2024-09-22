b55c2fb0387dbae7fc2ccb45d419dff7d2b93e88
sazarcode <cerberusprogrammer@gmail.com>
2024-09-22 10:09:41 -0700

    Refactor RoutineCompleteView to include ExerciseStatsList and RoutineInfo widgets

--- lib/routine-complete.view.dart
+++ lib/routine-complete.view.dart
@@ -1,6 +1,7 @@
-// routine_complete_view.dart
+import 'package:clean_abs/config/models/exercise_stat.dart';
 import 'package:clean_abs/config/models/routine_stat.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class RoutineCompleteView extends StatelessWidget {
   final RoutineStat routineStat;
@@ -15,50 +16,9 @@ class RoutineCompleteView extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Routine: ${routineStat.routine.name}',
-                style: const TextStyle(fontSize: 24)),
-            const SizedBox(height: 10),
-            Text(
-              'Total Duration: ${routineStat.totalDuration.inMinutes} minutes',
-              style: const TextStyle(fontSize: 18),
-            ),
-            Text(
-              'Calories Burned: ${routineStat.caloriesBurned}',
-              style: const TextStyle(fontSize: 18),
-            ),
-            Text(
-              'Start Time: ${routineStat.startTime}',
-              style: const TextStyle(fontSize: 18),
-            ),
-            Text(
-              'End Time: ${routineStat.endTime}',
-              style: const TextStyle(fontSize: 18),
-            ),
+            RoutineInfo(routineStat: routineStat),
             const SizedBox(height: 20),
-            const Text(
-              'Exercise Stats:',
-              style: TextStyle(fontSize: 20),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: routineStat.exerciseStats.length,
-                itemBuilder: (context, index) {
-                  final exerciseStat = routineStat.exerciseStats[index];
-                  return ListTile(
-                    title: Text(exerciseStat.exercise.name),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                            'Duration: ${exerciseStat.duration.inSeconds} seconds'),
-                        Text('Start Time: ${exerciseStat.startTime}'),
-                        Text('End Time: ${exerciseStat.endTime}'),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
+            ExerciseStatsList(exerciseStats: routineStat.exerciseStats),
           ],
         ),
       ),
@@ -68,6 +28,83 @@ class RoutineCompleteView extends StatelessWidget {
         },
         child: const Icon(Icons.done),
       ),
+    );
+  }
+}
+
+class ExerciseStatsList extends StatelessWidget {
+  final List<ExerciseStat> exerciseStats;
+
+  const ExerciseStatsList({super.key, required this.exerciseStats});
+
+  String _formatDateTime(DateTime dateTime) {
+    final DateFormat formatter = DateFormat('MMM dd yyyy hh:mm a');
+    return formatter.format(dateTime);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: exerciseStats.length,
+        itemBuilder: (context, index) {
+          final exerciseStat = exerciseStats[index];
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: ListTile(
+              title: Text(exerciseStat.exercise.name),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Duration: ${exerciseStat.duration.inSeconds} seconds'),
+                  Text(
+                      'Start Time: ${_formatDateTime(exerciseStat.startTime)}'),
+                  Text('End Time: ${_formatDateTime(exerciseStat.endTime)}'),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class RoutineInfo extends StatelessWidget {
+  final RoutineStat routineStat;
+
+  const RoutineInfo({super.key, required this.routineStat});
+
+  String _formatDateTime(DateTime dateTime) {
+    final DateFormat formatter = DateFormat('MMM dd yyyy hh:mm a');
+    return formatter.format(dateTime);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Routine: ${routineStat.routine.name}',
+            style: const TextStyle(fontSize: 24)),
+        const SizedBox(height: 10),
+        Text(
+          'Total Duration: ${routineStat.totalDuration.inMinutes} minutes',
+          style: const TextStyle(fontSize: 18),
+        ),
+        Text(
+          'Calories Burned: ${routineStat.caloriesBurned}',
+          style: const TextStyle(fontSize: 18),
+        ),
+        Text(
+          'Start Time: ${_formatDateTime(routineStat.startTime)}',
+          style: const TextStyle(fontSize: 18),
+        ),
+        Text(
+          'End Time: ${_formatDateTime(routineStat.endTime)}',
+          style: const TextStyle(fontSize: 18),
+        ),
+      ],
     );
   }
 }

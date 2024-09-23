672f2995ac88aae7896137a3bbc30b8ff6f6a8a9
sazarcode <cerberusprogrammer@gmail.com>
2024-09-22 18:56:30 -0700

    Refactor Exercise model serialization and deserialization

--- lib/config/models/exercise.dart
+++ lib/config/models/exercise.dart
@@ -12,4 +12,20 @@ class Exercise {
     required this.repetitions,
     required this.duration,
   });
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'description': description,
+        'image': image,
+        'repetitions': repetitions,
+        'duration': duration,
+      };
+
+  factory Exercise.fromJson(Map<String, dynamic> json) => Exercise(
+        name: json['name'],
+        description: json['description'],
+        image: json['image'],
+        repetitions: json['repetitions'],
+        duration: json['duration'],
+      );
 }

967712d13b2deca5fcccd9139aa92e76ba7b874a
sazarcode <cerberusprogrammer@gmail.com>
2024-09-22 18:56:40 -0700

    Refactor RoutineStatsNotifier to save and load routine stats using SharedPreferences

--- lib/providers/routine_provider.dart
+++ lib/providers/routine_provider.dart
@@ -1,11 +1,31 @@
+import 'dart:convert';
 import 'package:clean_abs/config/models/routine_stat.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RoutineStatsNotifier extends StateNotifier<List<RoutineStat>> {
-  RoutineStatsNotifier() : super([]);
+  RoutineStatsNotifier() : super([]) {
+    _loadRoutineStats();
+  }
 
   void addRoutineStat(RoutineStat routineStat) {
     state = [...state, routineStat];
+    _saveRoutineStats();
+  }
+
+  Future<void> _saveRoutineStats() async {
+    final prefs = await SharedPreferences.getInstance();
+    final routineStatsJson =
+        state.map((stat) => jsonEncode(stat.toJson())).toList();
+    await prefs.setStringList('routineStats', routineStatsJson);
+  }
+
+  Future<void> _loadRoutineStats() async {
+    final prefs = await SharedPreferences.getInstance();
+    final routineStatsJson = prefs.getStringList('routineStats') ?? [];
+    state = routineStatsJson
+        .map((json) => RoutineStat.fromJson(jsonDecode(json)))
+        .toList();
   }
 }
 

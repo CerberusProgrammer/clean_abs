41153f487c8374272800dbd7959e7afebc612e72
sazarcode <cerberusprogrammer@gmail.com>
2024-09-29 07:22:56 -0700

    Refactor Exercise class to include a copyWith method

--- lib/config/models/exercise.dart
+++ lib/config/models/exercise.dart
@@ -15,6 +15,24 @@ class Exercise {
     required this.duration,
   });
 
+  Exercise copyWith({
+    String? name,
+    String? description,
+    String? startImage,
+    String? finalImage,
+    int? repetitions,
+    int? duration,
+  }) {
+    return Exercise(
+      name: name ?? this.name,
+      description: description ?? this.description,
+      startImage: startImage ?? this.startImage,
+      finalImage: finalImage ?? this.finalImage,
+      repetitions: repetitions ?? this.repetitions,
+      duration: duration ?? this.duration,
+    );
+  }
+
   Map<String, dynamic> toJson() => {
         'name': name,
         'description': description,

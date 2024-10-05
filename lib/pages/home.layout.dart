29feb17fd2a3f67383b3d72b23644cf311e8955f
sazarcode <cerberusprogrammer@gmail.com>
2024-10-05 16:21:36 -0700

    Refactor HomeLayout to use dynamic title and body

--- lib/pages/home.layout.dart
+++ lib/pages/home.layout.dart
@@ -1,24 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeLayout extends StatelessWidget {
-  final Widget children;
+  final String? title;
+  final Widget? body;
 
-  const HomeLayout({super.key, required this.children});
+  const HomeLayout({
+    super.key,
+    this.body,
+    this.title,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Clean Abs'),
+        title: Text(title ?? 'Clean Abs'),
       ),
+      body: body,
       bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (index) {
+          switch (index) {
+            case 0:
+              context.go('/');
+              break;
+            case 1:
+              context.go('/stats');
+              break;
+          }
+        },
         destinations: const [
           NavigationDestination(
             icon: Icon(Icons.home),
             label: 'Home',
           ),
           NavigationDestination(
-            icon: Icon(Icons.search),
+            icon: Icon(Icons.bar_chart),
             label: 'Stats',
           ),
         ],

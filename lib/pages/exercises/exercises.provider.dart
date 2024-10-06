2312e0867f723cd1c4333198ed11044f8fcbae05
Omar Flores <cerberusprogrammer@gmail.com>
2024-10-06 08:25:22 -0700

    Refactor ExercisesNotifier to use ExercisesState and add exercisesProvider

--- lib/pages/exercises/exercises.provider.dart
+++ lib/pages/exercises/exercises.provider.dart
@@ -1,6 +1,19 @@
-import 'package:clean_abs/config/models/exercise.dart';
+import 'package:clean_abs/config/data/exercises.dart';
+import 'package:clean_abs/pages/exercises/exercises.state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ExercisesNotifier extends StateNotifier<List<Exercise>> {
-  ExercisesNotifier(super.initialExercises);
+final exercisesProvider =
+    StateNotifierProvider<ExercisesNotifier, ExercisesState>((ref) {
+  return ExercisesNotifier(ref);
+});
+
+class ExercisesNotifier extends StateNotifier<ExercisesState> {
+  final Ref ref;
+
+  ExercisesNotifier(this.ref)
+      : super(ExercisesState(
+          exercises: exercises,
+          isLoading: false,
+          hasError: false,
+        ));
 }
